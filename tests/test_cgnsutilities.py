015d7a7cb74732b55b10bf953dc207d8964f5d48
Eirikur Jonsson <eirikurj@umich.edu>
2021-08-09 14:32:18 +0000

    Adding tests for new getNumCells and getNumNodes functions

--- tests/test_cgnsutilities.py
+++ tests/test_cgnsutilities.py
@@ -90,3 +90,28 @@ class TestCLI(unittest.TestCase):
         self.assertFalse(out.returncode)
         self.assertTrue(os.path.isfile("717_wl_L2_overwriteFamilies.cgns"))
         os.remove("717_wl_L2_overwriteFamilies.cgns")
+
+
+class TestBlock(unittest.TestCase):
+    def setUp(self):
+        self.grid = readGrid(os.path.abspath(os.path.join(baseDir, "../examples/717_wl_L2.cgns")))
+
+    def test_getNumCells(self, train=False):
+        refFile = os.path.join(baseDir, "ref", "block_getNumCells.ref")
+        with BaseRegTest(refFile, train=train) as handler:
+            # Just pick the first block from the grid to test
+            numCells = self.grid.blocks[0].getNumCells()
+            handler.root_add_val("Number of cells", numCells, tol=0)
+
+    def train_getNumCells(self):
+        self.test_getNumCells(train=True)
+
+    def test_getNumNodes(self, train=False):
+        refFile = os.path.join(baseDir, "ref", "block_getNumNodes.ref")
+        with BaseRegTest(refFile, train=train) as handler:
+            # Just pick the first block from the grid to test
+            numCells = self.grid.blocks[0].getNumNodes()
+            handler.root_add_val("Number of nodes in the first block", numCells, tol=0)
+
+    def train_getNumNodes(self):
+        self.test_getNumNodes(train=True)
