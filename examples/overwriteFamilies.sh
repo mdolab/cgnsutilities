4f6c4b70cd70d013ffb22cc3e8927a28632fb3f4
nggw <nggw@umich.edu>
2021-12-15 17:43:05 -0500

    Add comment about block no. to example for overwriteFamilies shell script

--- examples/overwriteFamilies.sh
+++ examples/overwriteFamilies.sh
@@ -8,6 +8,7 @@ WRITEFILE=true
 
 # Prepare the family file input. Each line has the following format:
 # <block number> <faceID> <family>
+# If using a volume mesh, then <block number> is the domain number from tecplot, not the zone number
 
 if [[ $WRITEFILE -eq true ]]; then
 cat << 'EOF' > $FAMFILE
